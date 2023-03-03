8c75dac90f56d42aacf31c56d463a6e984f258a1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-03-03 10:16:23 +0700

    chore: format using black

--- Tugas 3/main.py
+++ Tugas 3/main.py
@@ -5,25 +5,27 @@ import time
 from PIL import Image
 
 st.set_page_config(
-	page_title="Python Web Assignment",
-	page_icon="🐍",
-	layout="wide",
-	initial_sidebar_state="expanded",
-	menu_items={
-		'Get Help': 'https://www.extremelycoolapp.com/help',
-		'Report a bug': "https://www.extremelycoolapp.com/bug",
-		'About': "# This is a header. This is an *extremely* cool app!"
-	}
+    page_title="Python Web Assignment",
+    page_icon="🐍",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
 )
 
 # 1. Write and Magic
 st.title("1. Write and Magic")
 st.write("Hello world!")
-df = pd.DataFrame({
-	'first column': [1, 2, 3, 4],
-	'second column': [10, 20, 30, 40],
-})
-df # streamlit magic
+df = pd.DataFrame(
+    {
+        "first column": [1, 2, 3, 4],
+        "second column": [10, 20, 30, 40],
+    }
+)
+df  # streamlit magic
 
 st.write(df)
 
@@ -34,31 +36,31 @@ st.code("let streamlit = 'Streamlit is awesome!'")
 
 # 3. Data Display Elements
 st.title("3. Data Display Elements")
-st.json({
-	'foo': 'bar',
-	'baz': 'boz',
-	'stuff': [
-		'stuff 1',
-		'stuff 2',
-		'stuff 3',
-		'stuff 5',
-	],
-})
+st.json(
+    {
+        "foo": "bar",
+        "baz": "boz",
+        "stuff": [
+            "stuff 1",
+            "stuff 2",
+            "stuff 3",
+            "stuff 5",
+        ],
+    }
+)
 
 # 4. Chart Elements
 st.title("4. Chart Elements")
-chart_data = pd.DataFrame(
-	np.random.randn(20, 3),
-	columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
 
 # 5. Input Widget
 st.title("5. Input Widget")
-agree = st.checkbox('Checked me if you are an awesome programmer!')
+agree = st.checkbox("Checked me if you are an awesome programmer!")
 
 if agree:
-	st.write('Great!')
+    st.write("Great!")
 
 # 6. Media Elements
 st.title("6. Media Elements")
@@ -68,28 +70,28 @@ st.image(img, caption="cool cat with sunglasses")
 # 7. Layouts and Containers
 st.title("7. Layouts and Containers")
 with st.sidebar:
-	with st.echo():
-		st.write("This code will be printed to the sidebar.")
+    with st.echo():
+        st.write("This code will be printed to the sidebar.")
 
-	with st.spinner("Loading..."):
-		time.sleep(5)
-	st.success("Done!")
+    with st.spinner("Loading..."):
+        time.sleep(5)
+    st.success("Done!")
 
 # 8. Status Element
 st.title("8. Status Element")
-st.error('[ERROR]: Bug will always haunt you', icon="🪲")
+st.error("[ERROR]: Bug will always haunt you", icon="🪲")
 
 # 9. Control Flow
 st.title("9. Control Flow")
 with st.form("my_form"):
-	st.write("Inside the form")
-	slider_val = st.slider("Form slider")
-	checkbox_val = st.checkbox("Form checkbox")
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
 
-	# Every form must have a submit button.
-	submitted = st.form_submit_button("Submit")
-	if submitted:
-		st.write("slider", slider_val, "checkbox", checkbox_val)
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
 
 st.write("Outside the form")
 
